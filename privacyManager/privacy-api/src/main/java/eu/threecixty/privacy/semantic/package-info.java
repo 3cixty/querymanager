b0fa46c2a46a6573e530ee4cc6dd85f413745051
kinh <congkinh.nguyen@inria.fr>
2014-05-28 16:48:31 +0200

    move to prifiler

--- privacyManager/privacy-api/src/main/java/eu/threecixty/privacy/semantic/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
-<p> Supports semantic annotations of Java classes through 
-    a set of basic interfaces to be implemented by those classes. </p>
-*/
-package eu.threecixty.privacy.semantic;
--- privacyManager/privacy-api/src/main/java/eu/threecixty/privacy/soa/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package defines an abstraction for Service Oriented Architecture based
- * on the semantic objects from the package eu.threecixty.privacy.semantic.
- * 
- * This semantic representation of a service should be mapped to a 
- * business-to-business or business-to-consumer service, like a Web Service, or
- * even used for an internal service.
- */
-package eu.threecixty.privacy.soa;
