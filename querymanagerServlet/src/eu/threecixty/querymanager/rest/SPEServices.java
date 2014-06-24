b86479bb347a7e1000ebd5da9cbd75762ef1d0ca
kinh <congkinh.nguyen@inria.fr>
2014-06-24 21:18:19 +0200

    now, RestAPIs to read and save profile are available

--- querymanagerServlet/src/eu/threecixty/querymanager/rest/SPEServices.java
+++ querymanagerServlet/src/eu/threecixty/querymanager/rest/SPEServices.java
@@ -2,6 +2,8 @@ package eu.threecixty.querymanager.rest;
 
 import java.net.HttpURLConnection;
 
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -18,21 +20,28 @@ import eu.threecixty.profile.ProfileInformationStorage;
 public class SPEServices {
 
 	
-	@POST
+	@GET
 	@Path("/getProfile/{accessToken}")
-	@Produces("application/json")
-	public ProfileInformation getProfile(@PathParam("accessToken") String accessToken) {
-		String uid = GoogleAccountUtils.getUID(accessToken);
-		if (uid == null || uid.equals("")) throw new WebApplicationException(HttpURLConnection.HTTP_BAD_REQUEST);
-		ProfileInformation profile = ProfileInformationStorage.loadProfile(uid);
-		if (profile == null) throw new WebApplicationException(HttpURLConnection.HTTP_BAD_REQUEST);
-		return profile;
+	@Produces("text/plain")
+	public String getProfile(@PathParam("accessToken") String accessToken) {
+		try {
+			String uid = GoogleAccountUtils.getUID(accessToken);
+			if (uid == null || uid.equals("")) throw new WebApplicationException(HttpURLConnection.HTTP_BAD_REQUEST);
+			ProfileInformation profile = ProfileInformationStorage.loadProfile(uid);
+			if (profile == null) throw new WebApplicationException(HttpURLConnection.HTTP_BAD_REQUEST);
+			Gson gson = new Gson();
+			String ret = gson.toJson(profile);
+			return ret;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		throw new WebApplicationException(HttpURLConnection.HTTP_BAD_REQUEST);
 	}
 
 	@POST
-	@Path("/saveProfile/{accessToken}/{profile}")
+	@Path("/saveProfile")
 	@Produces("text/plain")
-	public boolean saveProfile(@PathParam("accessToken") String accessToken, @PathParam("profile") String profileStr) {
+	public boolean saveProfile(@FormParam("accessToken") String accessToken, @FormParam("profile") String profileStr) {
 		String uid = GoogleAccountUtils.getUID(accessToken);
 		if (uid == null || uid.equals("")) throw new WebApplicationException(HttpURLConnection.HTTP_BAD_REQUEST);
 		if (profileStr == null || profileStr.equals("")) throw new WebApplicationException(HttpURLConnection.HTTP_BAD_REQUEST);
@@ -40,10 +49,29 @@ public class SPEServices {
 		try {
 		    ProfileInformation profile = gson.fromJson(profileStr, ProfileInformation.class);
 		    if (profile == null) throw new WebApplicationException(HttpURLConnection.HTTP_BAD_REQUEST);
+		    profile.setUid(uid);
 		    return ProfileInformationStorage.saveProfile(profile);
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new WebApplicationException(HttpURLConnection.HTTP_BAD_REQUEST);
 		}
+	}
+	
+	@POST
+	@Path("/getUID")
+	@Produces("text/plain")
+	public String getUID(@FormParam("accessToken") String accessToken) {
+		String uid = GoogleAccountUtils.getUID(accessToken);
+		if (uid == null || uid.equals("")) return "";
+		return uid;
+	}
+	
+	@POST
+	@Path("/validate")
+	@Produces("text/plain")
+	public boolean validate(@FormParam("accessToken") String accessToken) {
+		String uid = GoogleAccountUtils.getUID(accessToken);
+		if (uid == null || uid.equals("")) return false;
+		return true;
 	}
 }
