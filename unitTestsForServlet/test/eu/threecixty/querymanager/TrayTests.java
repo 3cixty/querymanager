93e10dbcfc6319340516e21c742888837323fc5d
kinh <congkinh.nguyen@inria.fr>
2014-07-02 16:46:52 +0200

    updated TrayTest with AppKey

--- unitTestsForServlet/test/eu/threecixty/querymanager/TrayTests.java
+++ unitTestsForServlet/test/eu/threecixty/querymanager/TrayTests.java
@@ -7,14 +7,16 @@ import org.junit.Test;
 
 public class TrayTests extends HTTPCall {
 
-	private static final String TRAY_SERVLET = "http://localhost:8080/querymanagerServlet-1.0/trayServlet";
+	private static final String TRAY_SERVLET = "http://3cixty.com:8080/querymanagerServlet-1.0/trayServlet";
 //	private static final String TRAY_SERVLET = "http://localhost:8080/querymanagerServlet/trayServlet";
+	private static final String APP_KEY="MTAzOTE4MTMwOTc4MjI2ODMyNjkwMTQwNDMxMTM5Nzg3MikoZXMJaWxfd3BxZAVs";
+	
 	
 	@Test
 	public void testAdd() {
 		try {
 			long currentTime = System.currentTimeMillis();
-			sendPost(TRAY_SERVLET,"action=add_tray_element&element_id=http://data.linkedevents.org/event/000ad3b7-98ce-4865-b2f8-efbec380a06d " + currentTime + "&element_type=event&token=ya29.LgATIqu78NH5Fx8AAACwO8g1qmGzzupZ4ms0OifXu4oIV79YZl0dNkPVon5pXA&source=ExlorationApp");
+			sendPost(TRAY_SERVLET,"action=add_tray_element&element_id=http://data.linkedevents.org/event/000ad3b7-98ce-4865-b2f8-efbec380a06d " + currentTime + "&element_type=event&token=ya29.NgB134Lz5ZYhMCEAAABNr4HN2YF_f3HX_kMpOlzt-iSJfcgiaxjQQhRAQdeSGRpJxv99wmvDmVo49MjOsys&source=ExlorationApp&key=" + APP_KEY);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -26,12 +28,12 @@ public class TrayTests extends HTTPCall {
 		try {
 			long currentTime = System.currentTimeMillis();
 			String uid = "TestUID" + currentTime;
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=001&element_type=event&token=" + uid + "&source=ExlorationApp");
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=002&element_type=event&token=" + uid + "&source=ExlorationApp");
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=003&element_type=event&token=" + uid + "&source=ExlorationApp");
+			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=001&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
+			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=002&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
+			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=003&element_type=event&token=" + uid + "&source=ExlorationApp&key="+ APP_KEY);
 			
 			
-			sendPost(TRAY_SERVLET, "action=get_tray_elements&google_token=" + uid, response);
+			sendPost(TRAY_SERVLET, "action=get_tray_elements&token=" + uid + "&key=" + APP_KEY, response);
 			
 			JSONArray arr = new JSONArray(response.toString());
 			Assert.assertTrue(arr.length() == 3);
@@ -51,15 +53,15 @@ public class TrayTests extends HTTPCall {
 		try {
 			long currentTime = System.currentTimeMillis();
 			String uid = "TestUID" + currentTime;
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=004&element_type=event&token=" + uid + "&source=ExlorationApp");
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=005&element_type=event&token=" + uid + "&source=ExlorationApp");
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=006&element_type=event&token=" + uid + "&source=ExlorationApp");
+			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=004&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
+			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=005&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
+			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=006&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
 			
 			
-			sendPost(TRAY_SERVLET, "action=update_tray_element&token=" + uid + "&element_id=004&element_type=event&source=ExlorationApp&attend=true");
+			sendPost(TRAY_SERVLET, "action=update_tray_element&token=" + uid + "&element_id=004&element_type=event&source=ExlorationApp&attend=true&key="+ APP_KEY);
 
 
-			sendPost(TRAY_SERVLET, "action=get_tray_elements&google_token=" + uid, response);
+			sendPost(TRAY_SERVLET, "action=get_tray_elements&token=" + uid + "&key="+ APP_KEY, response);
 			
 			JSONArray arr = new JSONArray(response.toString());
 			Assert.assertTrue(arr.length() == 3);
@@ -68,11 +70,11 @@ public class TrayTests extends HTTPCall {
 			Assert.assertTrue(arr.getJSONObject(1).getString("itemId").equals("006"));
 			Assert.assertTrue(arr.getJSONObject(2).getString("itemId").equals("004")); // changed because of updating time
 			
-			sendPost(TRAY_SERVLET, "action=update_tray_element&token=" + uid + "&element_id=004&element_type=event&attend=true&source=ExlorationApp&delete=true");
+			sendPost(TRAY_SERVLET, "action=update_tray_element&token=" + uid + "&element_id=004&element_type=event&attend=true&source=ExlorationApp&delete=true&key=" + APP_KEY);
 			
 			response = new StringBuffer();
 			
-			sendPost(TRAY_SERVLET, "action=get_tray_elements&google_token=" + uid, response);
+			sendPost(TRAY_SERVLET, "action=get_tray_elements&token=" + uid + "&key=" + APP_KEY, response);
 			arr = new JSONArray(response.toString());
 			Assert.assertTrue(arr.length() == 2);
 			
@@ -87,21 +89,21 @@ public class TrayTests extends HTTPCall {
 		try {
 			long currentTime = System.currentTimeMillis();
 			String uid = "TestUID" + currentTime;
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=007&element_type=event&token=" + uid + "&source=ExlorationApp");
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=008&element_type=event&token=" + uid + "&source=ExlorationApp");
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=009&element_type=event&token=" + uid + "&source=ExlorationApp");
+			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=007&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
+			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=008&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
+			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=009&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
 			
 			
-			sendPost(TRAY_SERVLET, "action=get_tray_elements&google_token=" + uid, response);
+			sendPost(TRAY_SERVLET, "action=get_tray_elements&token=" + uid + "&key=" + APP_KEY, response);
 			
 			JSONArray arr = new JSONArray(response.toString());
 			Assert.assertTrue(arr.length() == 3);
 			
-			sendPost(TRAY_SERVLET, "action=empty_tray&token=" + uid);
+			sendPost(TRAY_SERVLET, "action=empty_tray&token=" + uid + "&key=" + APP_KEY);
 			
 			response = new StringBuffer();
 			
-			sendPost(TRAY_SERVLET, "action=get_tray_elements&google_token=" + uid, response);
+			sendPost(TRAY_SERVLET, "action=get_tray_elements&token=" + uid + "&key=" + APP_KEY, response);
 
 			arr = new JSONArray(response.toString());
 			Assert.assertTrue(arr.length() == 0);
