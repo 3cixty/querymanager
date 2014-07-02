dc07b81aaed6bcbca78d17bd51c894169e290436
kinh <congkinh.nguyen@inria.fr>
2014-07-02 22:38:38 +0200

    tray unit tests

--- unitTestsForServlet/test/eu/threecixty/querymanager/TrayTests.java
+++ unitTestsForServlet/test/eu/threecixty/querymanager/TrayTests.java
@@ -7,16 +7,18 @@ import org.junit.Test;
 
 public class TrayTests extends HTTPCall {
 
-	private static final String TRAY_SERVLET = "http://3cixty.com:8080/querymanagerServlet-1.0/trayServlet";
-//	private static final String TRAY_SERVLET = "http://localhost:8080/querymanagerServlet/trayServlet";
-	private static final String APP_KEY="MTAzOTE4MTMwOTc4MjI2ODMyNjkwMTQwNDMxMTM5Nzg3MikoZXMJaWxfd3BxZAVs";
+//	private static final String TRAY_SERVLET = "http://3cixty.com:8080/querymanagerServlet-1.0/trayServlet";
+//	private static final String APP_KEY="MTAzOTE4MTMwOTc4MjI2ODMyNjkwMTQwNDMxMTM5Nzg3MikoZXMJaWxfd3BxZAVs";
+	private static final String TRAY_SERVLET = "http://localhost:8080/querymanagerServlet-1.0/trayServlet";
+	private static final String APP_KEY="MTAzOTE4MTMwOTc4MjI2ODMyNjkwMTQwNDMyODQ1NDY4OWt1Y2Zqbl9kdHRhLXBx";
 	
 	
 	@Test
 	public void testAdd() {
 		try {
 			long currentTime = System.currentTimeMillis();
-			sendPost(TRAY_SERVLET,"action=add_tray_element&element_id=http://data.linkedevents.org/event/000ad3b7-98ce-4865-b2f8-efbec380a06d " + currentTime + "&element_type=event&token=ya29.NgB134Lz5ZYhMCEAAABNr4HN2YF_f3HX_kMpOlzt-iSJfcgiaxjQQhRAQdeSGRpJxv99wmvDmVo49MjOsys&source=ExlorationApp&key=" + APP_KEY);
+			String params = createTray("add_tray_element", "http://data.linkedevents.org/event/000ad3b7-98ce-4865-b2f8-efbec380a06d " + currentTime, "event", "ExlorationApp", "ya29.NgBcSL5m75lSmiEAAAAKurNghpKPkcriWULzU-EC5JhQAye9t5AAy-BfPW4-usgHu90gjv4nF7BKHhkSvbY");
+			sendPost(TRAY_SERVLET, params);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -28,19 +30,26 @@ public class TrayTests extends HTTPCall {
 		try {
 			long currentTime = System.currentTimeMillis();
 			String uid = "TestUID" + currentTime;
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=001&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=002&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=003&element_type=event&token=" + uid + "&source=ExlorationApp&key="+ APP_KEY);
 			
+			String params1 = createTray("add_tray_element", "001", "event", "ExlorationApp", uid);
+			sendPost(TRAY_SERVLET, params1);
 			
-			sendPost(TRAY_SERVLET, "action=get_tray_elements&token=" + uid + "&key=" + APP_KEY, response);
+			String params2 = createTray("add_tray_element", "002", "event", "ExlorationApp", uid);
+			sendPost(TRAY_SERVLET, params2);
+			
+			String params3 = createTray("add_tray_element", "003", "event", "ExlorationApp", uid);
+			sendPost(TRAY_SERVLET, params3);
+						
+			String params4 = createTray("get_tray_elements", null, null, null, uid);
+			
+			sendPost(TRAY_SERVLET, params4, response);
 			
 			JSONArray arr = new JSONArray(response.toString());
 			Assert.assertTrue(arr.length() == 3);
 			
-			Assert.assertTrue(arr.getJSONObject(0).getString("itemId").equals("001"));
-			Assert.assertTrue(arr.getJSONObject(1).getString("itemId").equals("002"));
-			Assert.assertTrue(arr.getJSONObject(2).getString("itemId").equals("003"));
+			Assert.assertTrue(arr.getJSONObject(0).getString("element_id").equals("001"));
+			Assert.assertTrue(arr.getJSONObject(1).getString("element_id").equals("002"));
+			Assert.assertTrue(arr.getJSONObject(2).getString("element_id").equals("003"));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -53,28 +62,39 @@ public class TrayTests extends HTTPCall {
 		try {
 			long currentTime = System.currentTimeMillis();
 			String uid = "TestUID" + currentTime;
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=004&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=005&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=006&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
 			
+			String params4 = createTray("add_tray_element", "004", "event", "ExlorationApp", uid);
+			sendPost(TRAY_SERVLET, params4);
 			
-			sendPost(TRAY_SERVLET, "action=update_tray_element&token=" + uid + "&element_id=004&element_type=event&source=ExlorationApp&attend=true&key="+ APP_KEY);
+			String params5 = createTray("add_tray_element", "005", "event", "ExlorationApp", uid);
+			sendPost(TRAY_SERVLET, params5);
+			
+			String params6 = createTray("add_tray_element", "006", "event", "ExlorationApp", uid);
+			sendPost(TRAY_SERVLET, params6);
+			
+			params4 = createTray("update_tray_element", "004", "event", "ExlorationApp", uid, true);
+			sendPost(TRAY_SERVLET, params4);
+			
 
-
-			sendPost(TRAY_SERVLET, "action=get_tray_elements&token=" + uid + "&key="+ APP_KEY, response);
+			String paramsList = createTray("get_tray_elements", null, null, null, uid);
+			
+			sendPost(TRAY_SERVLET, paramsList, response);
 			
 			JSONArray arr = new JSONArray(response.toString());
 			Assert.assertTrue(arr.length() == 3);
 			
-			Assert.assertTrue(arr.getJSONObject(0).getString("itemId").equals("005"));
-			Assert.assertTrue(arr.getJSONObject(1).getString("itemId").equals("006"));
-			Assert.assertTrue(arr.getJSONObject(2).getString("itemId").equals("004")); // changed because of updating time
+			Assert.assertTrue(arr.getJSONObject(0).getString("element_id").equals("005"));
+			Assert.assertTrue(arr.getJSONObject(1).getString("element_id").equals("006"));
+			Assert.assertTrue(arr.getJSONObject(2).getString("element_id").equals("004")); // changed because of updating time
 			
-			sendPost(TRAY_SERVLET, "action=update_tray_element&token=" + uid + "&element_id=004&element_type=event&attend=true&source=ExlorationApp&delete=true&key=" + APP_KEY);
+			// delete a tray
+			params4 = createTray("update_tray_element", "004", "event", "ExlorationApp", uid, true, true);
+			sendPost(TRAY_SERVLET, params4);
 			
 			response = new StringBuffer();
 			
-			sendPost(TRAY_SERVLET, "action=get_tray_elements&token=" + uid + "&key=" + APP_KEY, response);
+			sendPost(TRAY_SERVLET, paramsList, response);
+			
 			arr = new JSONArray(response.toString());
 			Assert.assertTrue(arr.length() == 2);
 			
@@ -89,21 +109,30 @@ public class TrayTests extends HTTPCall {
 		try {
 			long currentTime = System.currentTimeMillis();
 			String uid = "TestUID" + currentTime;
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=007&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=008&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
-			sendPost(TRAY_SERVLET, "action=add_tray_element&element_id=009&element_type=event&token=" + uid + "&source=ExlorationApp&key=" + APP_KEY);
 			
+			String params4 = createTray("add_tray_element", "004", "event", "ExlorationApp", uid);
+			sendPost(TRAY_SERVLET, params4);
 			
-			sendPost(TRAY_SERVLET, "action=get_tray_elements&token=" + uid + "&key=" + APP_KEY, response);
+			String params5 = createTray("add_tray_element", "005", "event", "ExlorationApp", uid);
+			sendPost(TRAY_SERVLET, params5);
+			
+			String params6 = createTray("add_tray_element", "006", "event", "ExlorationApp", uid);
+			sendPost(TRAY_SERVLET, params6);			
+			
+			String paramsList = createTray("get_tray_elements", null, null, null, uid);
+			
+			sendPost(TRAY_SERVLET, paramsList, response);
 			
 			JSONArray arr = new JSONArray(response.toString());
 			Assert.assertTrue(arr.length() == 3);
 			
-			sendPost(TRAY_SERVLET, "action=empty_tray&token=" + uid + "&key=" + APP_KEY);
+			
+			String paramsEmpty = createTray("empty_tray", null, null, null, uid);
+			sendPost(TRAY_SERVLET, paramsEmpty);
 			
 			response = new StringBuffer();
 			
-			sendPost(TRAY_SERVLET, "action=get_tray_elements&token=" + uid + "&key=" + APP_KEY, response);
+			sendPost(TRAY_SERVLET, paramsList, response);
 
 			arr = new JSONArray(response.toString());
 			Assert.assertTrue(arr.length() == 0);
@@ -111,5 +140,49 @@ public class TrayTests extends HTTPCall {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private String createTray(String action, String trayItemId, String trayType, String source, String token, boolean attended, boolean deleted) {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("{");
+		if (action != null) buffer.append("\"action\":").append("\"" + action + "\", ");
+		if (trayItemId != null) buffer.append("\"element_id\":").append("\"" + trayItemId + "\", ");
+		if (trayType != null) buffer.append("\"element_type\":").append("\"" + trayType + "\", ");
+		if (source != null) buffer.append("\"source\":").append("\"" + source + "\", ");
+		if (token != null) buffer.append("\"token\":").append("\"" + token + "\", ");
+		buffer.append("\"attend\":").append("\"" + attended + "\", ");
+		buffer.append("\"delete\":").append("\"" + deleted + "\", ");
+		buffer.append("\"key\":").append("\"" + APP_KEY + "\"");
+		
+		buffer.append("}");
+		return buffer.toString();		
+	}
+	
+	private String createTray(String action, String trayItemId, String trayType, String source, String token, boolean attended) {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("{");
+		if (action != null) buffer.append("\"action\":").append("\"" + action + "\", ");
+		if (trayItemId != null) buffer.append("\"element_id\":").append("\"" + trayItemId + "\", ");
+		if (trayType != null) buffer.append("\"element_type\":").append("\"" + trayType + "\", ");
+		if (source != null) buffer.append("\"source\":").append("\"" + source + "\", ");
+		if (token != null) buffer.append("\"token\":").append("\"" + token + "\", ");
+		buffer.append("\"attend\":").append("\"" + attended + "\", ");
+		buffer.append("\"key\":").append("\"" + APP_KEY + "\"");
+		
+		buffer.append("}");
+		return buffer.toString();		
+	}
+	
+	private String createTray(String action, String trayItemId, String trayType, String source, String token) {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("{");
+		if (action != null) buffer.append("\"action\":").append("\"" + action + "\", ");
+		if (trayItemId != null) buffer.append("\"element_id\":").append("\"" + trayItemId + "\", ");
+		if (trayType != null) buffer.append("\"element_type\":").append("\"" + trayType + "\", ");
+		if (source != null) buffer.append("\"source\":").append("\"" + source + "\", ");
+		if (token != null) buffer.append("\"token\":").append("\"" + token + "\", ");
+		buffer.append("\"key\":").append("\"" + APP_KEY + "\"");
+		buffer.append("}");
+		return buffer.toString();
 	}
 }
