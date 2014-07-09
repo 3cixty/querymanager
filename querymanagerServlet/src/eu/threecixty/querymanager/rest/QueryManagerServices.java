1c09ad2d56cde61186e83f98a93b1011a5efcfbe
kinh <congkinh.nguyen@inria.fr>
2014-07-09 14:44:21 +0200

    refactoring

--- querymanagerServlet/src/eu/threecixty/querymanager/rest/QueryManagerServices.java
+++ querymanagerServlet/src/eu/threecixty/querymanager/rest/QueryManagerServices.java
@@ -296,4 +296,27 @@ public class QueryManagerServices {
 		}
     	return "";
     }
+
+    public class KeyValuePair {
+
+    	private String groupBy;
+    	private String value;
+
+    	public String getGroupBy() {
+    		return groupBy;
+    	}
+
+    	public void setGroupBy(String groupBy) {
+    		this.groupBy = groupBy;
+    	}
+
+    	public String getValue() {
+    		return value;
+    	}
+
+    	public void setValue(String value) {
+    		this.value = value;
+    	}
+    }
+
 }
