b56eda9b2c3fee3de579fc76e9763c8af28b1fd9
kinh <congkinh.nguyen@inria.fr>
2014-07-21 17:25:58 +0200

    use 3cixty sparql endpoint

--- querymanager-java/src/eu/threecixty/querymanager/QueryManager.java
+++ querymanager-java/src/eu/threecixty/querymanager/QueryManager.java
@@ -32,8 +32,9 @@ import eu.threecixty.profile.oldmodels.Place;
 import eu.threecixty.profile.oldmodels.Preference;
 
  public class QueryManager implements IQueryManager {
-	 
-	 private static final String EVENTMEDIA_URL_PREFIX = "http://eventmedia.eurecom.fr/sparql?default-graph-uri=&query=";
+
+	 //private static final String EVENTMEDIA_URL_PREFIX = "http://eventmedia.eurecom.fr/sparql?default-graph-uri=&query=";
+	 private static final String EVENTMEDIA_URL_PREFIX = "http://3cixty.eurecom.fr/sparql?default-graph-uri=&query=";
 
 	 /**Current query*/
 	private ThreeCixtyQuery query;
