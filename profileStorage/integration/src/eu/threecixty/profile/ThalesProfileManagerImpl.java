dcfcd1c9ace8668153565d7c7e32b534bde2b82e
kinh <congkinh.nguyen@inria.fr>
2014-07-22 13:43:25 +0200

    add integration module to call privacyManager provided by Thales

---
+++ profileStorage/integration/src/eu/threecixty/profile/ThalesProfileManagerImpl.java
@@ -0,0 +1,125 @@
+package eu.threecixty.profile;
+
+import java.util.List;
+import java.util.Set;
+
+import eu.threecixty.profile.GpsCoordinateUtils.GpsCoordinate;
+
+public class ThalesProfileManagerImpl implements ProfileManager {
+
+	public UserProfile getProfile(String uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean saveProfile(UserProfile userProfile) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean existUID(String uid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int getMinimumNumberOfTimesVisited(String uid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public float getMinimumScoreRated(String uid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getMinimumNumberOfTimesVisitedForFriends(String uid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public float getMinimumScoreRatedForFriends(String uid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getCountryName(String uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTownName(String uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public GpsCoordinate getCoordinate(String uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getPlaceNamesFromRating(String uid, float rating) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getPlaceNamesFromNumberOfTimesVisited(String uid,
+			int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getPlaceNamesFromRatingOfFriends(String uid,
+			float rating) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getPlaceNamesFromNumberOfTimesVisitedOfFriends(
+			String uid, int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getEventNamesFromEventPreferences(String uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<StartAndEndDate> getPreferredStartAndEndDates(String uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getEventNamesFromRating(String uid, float rating) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getEventNamesFromNumberOfTimesVisited(String uid,
+			int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getEventNamesFromRatingOfFriends(String uid,
+			float rating) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getEventNamesFromNumberOfTimesVisitedOfFriends(
+			String uid, int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getEventNamesWhichFriendsLikeToVisit(String uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<IDMapping> getIDMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
