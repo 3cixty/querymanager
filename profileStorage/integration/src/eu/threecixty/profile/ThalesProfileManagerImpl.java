18f99aaf0d1987866035ac22fb4611086c093f44
kinh <congkinh.nguyen@inria.fr>
2014-07-24 14:43:24 +0200

    get country name, town name, gps information from PrivacyManager

--- profileStorage/integration/src/eu/threecixty/profile/ThalesProfileManagerImpl.java
+++ profileStorage/integration/src/eu/threecixty/profile/ThalesProfileManagerImpl.java
@@ -55,29 +55,31 @@ public class ThalesProfileManagerImpl implements ProfileManager {
 	}
 
 	public String getCountryName(String uid) {
-		// TODO: from 100900047095598983805Address, need to get countryname
-		ThalesProfileManagerAndSession profileMngAndSession = new ThalesProfileManagerAndSession(uid);
-		String vcardAddr = "vcard:hasAddress";
-		List<String> propertyPaths = new ArrayList<String>();
-		propertyPaths.add(vcardAddr);
-		try {
-			Collection<ValuedProperty> propertyValues =  profileMngAndSession.profileMgr.getProfileProperties(
-					profileMngAndSession.session, uid, propertyPaths);
-			if (propertyValues == null || propertyValues.size() == 0) return null;
-			return propertyValues.iterator().next().getValue(0); //  TODO: from 100900047095598983805Address, need to get countryname 
-		} catch (ProfileException e) {
-			e.printStackTrace();
-		}
-		return null;
+		return getInformationFromProfileAddress(uid, "vcard:country-name");
 	}
 
 	public String getTownName(String uid) {
-		// TODO Auto-generated method stub
-		return null;
+		return getInformationFromProfileAddress(uid, ":townName");
 	}
 
 	public GpsCoordinate getCoordinate(String uid) {
-		// TODO Auto-generated method stub
+		ThalesProfileManagerAndSession profileMngAndSession = new ThalesProfileManagerAndSession(uid);
+		String vcardLat = "vcard:hasAddress/vcard:latitude";
+		String vcardLon = "vcard:hasAddress/vcard:longitude";
+
+		List<String> propertyPaths = new ArrayList<String>();
+		propertyPaths.add(vcardLat);
+		propertyPaths.add(vcardLon);
+		try {
+			Collection<ValuedProperty> propertyValues =  profileMngAndSession.profileMgr.getProfileProperties(
+					profileMngAndSession.session, uid, propertyPaths);
+			if (propertyValues == null || propertyValues.size() <= 1) return null;
+			String latStr = propertyValues.iterator().next().getValue(0);
+			String lonStr = propertyValues.iterator().next().getValue(0);
+			return new GpsCoordinate(getDoubleFromXSDValue(latStr), getDoubleFromXSDValue(lonStr));
+		} catch (ProfileException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
@@ -145,5 +147,36 @@ public class ThalesProfileManagerImpl implements ProfileManager {
 	public Set<IDMapping> getIDMappings() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	/**
+	 * Gets address information.
+	 * @param uid
+	 * 				Google UID
+	 * @param concept
+	 * 				The concept, for example <code>vcard:country-name</code>, <code>:townName</code>, etc.
+	 * @return Corresponding information about address if there exists, <code>null</code> otherwise.
+	 */
+	private String getInformationFromProfileAddress(String uid, String concept) {
+		ThalesProfileManagerAndSession profileMngAndSession = new ThalesProfileManagerAndSession(uid);
+		String vcardAddr = "vcard:hasAddress/" + concept;
+		List<String> propertyPaths = new ArrayList<String>();
+		propertyPaths.add(vcardAddr);
+		try {
+			Collection<ValuedProperty> propertyValues =  profileMngAndSession.profileMgr.getProfileProperties(
+					profileMngAndSession.session, uid, propertyPaths);
+			if (propertyValues == null || propertyValues.size() == 0) return null;
+			return propertyValues.iterator().next().getValue(0);
+		} catch (ProfileException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private double getDoubleFromXSDValue(String doubleInXSDValue) {
+		int index = doubleInXSDValue.indexOf("^^");
+		if (index < 0) throw new RuntimeException("Invalid value");
+		String str = doubleInXSDValue.substring(0, index);
+		return Double.parseDouble(str);
 	}
 }
