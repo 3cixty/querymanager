dcd8467807d1a5afcbbc83556e645e1cfac06146
kinh <congkinh.nguyen@inria.fr>
2014-08-20 10:44:39 +0200

    extract clientId and clientSecret to add into redirect_uri's params

--- oauth/apis-authorization-server/src/main/java/org/surfnet/oaaas/auth/AuthenticationFilter.java
+++ oauth/apis-authorization-server/src/main/java/org/surfnet/oaaas/auth/AuthenticationFilter.java
@@ -80,7 +80,8 @@ public class AuthenticationFilter implements Filter {
   protected AuthorizationRequest extractAuthorizationRequest(HttpServletRequest request) {
     String responseType = request.getParameter("response_type");
     String clientId = request.getParameter("client_id");
-    String redirectUri = request.getParameter("redirect_uri");
+    String clientSecret = request.getParameter("clientSecret");
+    String redirectUri = request.getParameter("redirect_uri") + "?clientId=" + clientId + "&clientSecret=" + clientSecret;
 
     List<String> requestedScopes = null;
     if (StringUtils.isNotBlank(request.getParameter("scope"))) {
