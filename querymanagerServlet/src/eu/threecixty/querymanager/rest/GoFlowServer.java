2c8b220a972dd14e39310e37ac0127eed860a2ff
kinh <congkinh.nguyen@inria.fr>
2014-10-09 15:55:03 +0200

    class created for the issue https://github.com/3cixty/querymanager/issues/38

---
+++ querymanagerServlet/src/eu/threecixty/querymanager/rest/GoFlowServer.java
@@ -0,0 +1,55 @@
+package eu.threecixty.querymanager.rest;
+
+public class GoFlowServer {
+
+	private static final Object _sync = new Object();
+	private static GoFlowServer instance;
+
+	public static GoFlowServer getInstance() {
+		if (instance == null) {
+			synchronized (_sync) {
+				if (instance == null) instance = new GoFlowServer();
+			}
+		}
+		return instance;
+	}
+
+	/**
+	 * This method returns <code>null</null> whenever it fails to create an account at GoFlow server ;
+	 * otherwise, this method returns the password of a given uid.
+	 * <br><br>
+	 * uid is Google user_id, for example 100900047095598983805. So,
+	 * please keep in mind a case where someone uses two different applications to decide whether 
+	 * you only provide an account for applications within the 3cixty platform or each application has
+	 * different account.
+	 *  
+	 *
+	 * @param appId
+	 * @param uid
+	 * @return
+	 */
+	public String createEndUser(String appId, String uid) {
+		// TODO: please implement this method to create an account at GoFlow server
+		return null;
+	}
+
+
+	/**
+	 * uid is Google user_id, for example 100900047095598983805. So,
+	 * please keep in mind a case where someone uses two different applications to decide whether 
+	 * you only provide an account for applications within the 3cixty platform or each application has
+	 * different account.
+	 * 
+	 * @param appId
+	 * @param uid
+	 * @return
+	 */
+	public String getEndUserPassword(String appId, String uid) {
+		// TODO: please implement this method to get the password of a given uid from GoFlow server
+		// Note that password 'null' means user doesn't exists
+		return null;
+	}
+	
+	private GoFlowServer() {
+	}
+}
