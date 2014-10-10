af80c22680e04dae9465050d212d6a99059a5619
kinh <congkinh.nguyen@inria.fr>
2014-10-10 15:19:49 +0200

    use virtuoso on dev server

--- querymanager-java/src/eu/threecixty/querymanager/QueryManager.java
+++ querymanager-java/src/eu/threecixty/querymanager/QueryManager.java
@@ -34,7 +34,8 @@ import eu.threecixty.profile.oldmodels.Rating;
 
  public class QueryManager implements IQueryManager {
 	 //private static final String EVENTMEDIA_URL_PREFIX = "http://eventmedia.eurecom.fr/sparql?default-graph-uri=&query=";
-	 private static final String EVENTMEDIA_URL_PREFIX = "http://3cixty.eurecom.fr/sparql?default-graph-uri=&query=";
+	 //private static final String EVENTMEDIA_URL_PREFIX = "http://3cixty.eurecom.fr/sparql?default-graph-uri=&query=";
+	 private static final String EVENTMEDIA_URL_PREFIX = "http://dev.3cixty.com/sparql?default-graph-uri=&query=";
 
 	 /**Original query*/
 	 private ThreeCixtyQuery originalQuery;
