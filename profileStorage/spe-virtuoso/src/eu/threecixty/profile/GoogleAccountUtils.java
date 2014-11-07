8cf6af6a57917baafd68723d4594583bbb55fa65
kinh <congkinh.nguyen@inria.fr>
2014-11-07 09:23:13 +0100

    rename

---
+++ profileStorage/spe-virtuoso/src/eu/threecixty/profile/GoogleAccountUtils.java
@@ -0,0 +1,145 @@
+package eu.threecixty.profile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import eu.threecixty.profile.ProfileManagerImpl;
+import eu.threecixty.profile.UserProfile;
+import eu.threecixty.profile.oldmodels.Name;
+
+/**
+ * Utility class to update account info.
+ *
+ * @author Rachit Agarwal
+ *
+ */
+public class GoogleAccountUtils {
+	
+//	/**
+//	 * Validates a given access token, 
+//   * then extract Google info (name, knows) to update UserProfile if there is
+//	 * no information about this user.
+//	 * @param accessToken
+//	 * @return public user ID if the given access token is valid. Otherwise, the method returns an empty string.
+//	 */
+
+
+	public synchronized static String getUID(String accessToken) {
+		if (accessToken == null) return "";
+		String user_id = null;
+		try {
+			// due to error asked by Christian
+//			String reqMsg = readUrl(
+//					"https://www.googleapis.com/plus/v1/people/me?access_token=" + accessToken);
+			String reqMsg = readUrl(
+					"https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + accessToken);
+			JSONObject json = new JSONObject(reqMsg);
+			user_id = json.getString("id");
+//			JSONObject nameObj = json.getJSONObject("name");
+			String givenName = json.getString("given_name");
+			String familyName = json.getString("family_name");
+			
+			if (ProfileManagerImpl.getInstance().existUID(user_id)) return user_id; // no need to update info as it exists
+			
+			UserProfile profile = new UserProfile();
+			profile.setHasUID(user_id);
+			Name name = new Name();
+			profile.setHasName(name);
+			name.setGivenName(givenName);
+			name.setFamilyName(familyName);
+
+			
+			// do this when login to 3cixty authorization say edit settings,
+			// select circles you want the app to get info form.
+			// then select only me.
+			// finaly say authourize.
+			reqMsg = readUrl("https://www.googleapis.com/plus/v1/people/me/people/visible?access_token="
+					+ accessToken);
+			json = new JSONObject(reqMsg);
+			
+			String nextPageToken = null;
+		 	Set<String> knows = new HashSet<String>();
+			
+		 	
+			if (json.has("nextPageToken")){		
+				while(json.has("nextPageToken")){
+					nextPageToken = json.getString("nextPageToken");
+					JSONArray jsonArray = json.getJSONArray("items");
+					int length=jsonArray.length();
+					for (int i = 0; i < length; i++) {
+							JSONObject jObject = jsonArray.getJSONObject(i);
+							knows.add(jObject.getString("id"));
+					}
+					reqMsg = readUrl("https://www.googleapis.com/plus/v1/people/me/people/visible?access_token="
+							+ accessToken+"&pageToken="+nextPageToken);
+					json = new JSONObject(reqMsg);
+				}
+			}
+			else{
+				JSONArray jsonArray = json.getJSONArray("items");
+				int length=jsonArray.length();
+				for (int i = 0; i < length; i++) {
+						JSONObject jObject = jsonArray.getJSONObject(i);
+						knows.add(jObject.getString("id"));
+				}
+			}			
+			
+			profile.setKnows(knows);
+
+			ProfileManagerImpl.getInstance().saveProfile(profile, "Insert");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (user_id == null) return "";
+		return user_id;
+	}
+	
+	public static int getValidationTime(String accessToken) {
+		try {
+			String reqMsg = readUrl(
+					"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=" + accessToken);
+			JSONObject json = new JSONObject(reqMsg);
+			int time = json.getInt("expires_in");
+			return time;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return 0;
+	}
+
+	/**
+	 * Gets content from a given URL string.
+	 *
+	 * @param urlString
+	 * @return
+	 * @throws Exception
+	 */
+	private static String readUrl(String urlString) throws Exception {
+	    BufferedReader reader = null;
+	    try {
+	        URL url = new URL(urlString);
+	        reader = new BufferedReader(new InputStreamReader(url.openStream()));
+	        StringBuffer buffer = new StringBuffer();
+	        int read;
+	        char[] chars = new char[1024];
+	        while ((read = reader.read(chars)) != -1)
+	            buffer.append(chars, 0, read); 
+
+	        return buffer.toString();
+	    } finally {
+	        if (reader != null)
+	            reader.close();
+	    }
+	}
+	
+	private GoogleAccountUtils() {
+	}
+}
---
+++ profileStorage/spe-virtuoso/src/eu/threecixty/profile/virtuosoConnection.java
@@ -0,0 +1,149 @@
+package eu.threecixty.profile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.DriverManager;
+import java.util.Properties;
+import java.sql.Connection;
+import java.sql.SQLException;
+import virtuoso.jena.driver.*;
+import virtuoso.jena.driver.VirtGraph;
+import virtuoso.jena.driver.VirtuosoQueryExecution;
+import virtuoso.jena.driver.VirtuosoQueryExecutionFactory;
+import virtuoso.jena.driver.VirtuosoUpdateFactory;
+import virtuoso.jena.driver.VirtuosoUpdateRequest;
+
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryFactory;
+
+public class virtuosoConnection {
+	// JDBC driver name and database URL
+		static String DB_URL;
+
+		// Database credentials
+		static String USER;
+		static String PASS;
+		
+		// Graph to query
+		static String GRAPH;
+		
+		/**
+		 * read config file (parameters for virtuoso)
+		 * @throws IOException
+		 */
+		public static Connection processConfigFile() throws IOException, SQLException {
+			Properties prop = new Properties();
+			String propfileName="conf.properties";
+			InputStream instream=Thread.currentThread().getContextClassLoader().getResourceAsStream(propfileName);
+			prop.load(instream);
+
+			if (prop.getProperty("virtuoso.address") != null) {
+				virtuosoConnection.DB_URL = prop.getProperty("virtuoso.address");
+			} else {
+				throw new IOException(
+						"The property virtuoso.address doesn't exists");
+			}
+
+			if (prop.getProperty("virtuoso.user") != null) {
+				virtuosoConnection.USER = prop.getProperty("virtuoso.user");
+			} else {
+				throw new IOException("The property virtuoso.user doesn't exists");
+			}
+
+			if (prop.getProperty("virtuoso.pass") != null) {
+				virtuosoConnection.PASS = prop.getProperty("virtuoso.pass");
+			} else {
+				throw new IOException("The property virtuoso.pass doesn't exists");
+			}
+			
+			if (prop.getProperty("virtuoso.graph") != null) {
+				virtuosoConnection.GRAPH = prop.getProperty("virtuoso.graph");
+			} else {
+				throw new IOException("The property virtuoso.graph doesn't exists");
+			}
+			try {
+				Class.forName("virtuoso.jdbc4.Driver");
+				return DriverManager.getConnection(virtuosoConnection.DB_URL,
+					virtuosoConnection.USER, virtuosoConnection.PASS);
+			}catch(SQLException ex){
+				ex.printStackTrace();
+				throw new SQLException("Connection not possible. This Service not available.");
+			}	
+		catch(ClassNotFoundException ex){
+			ex.printStackTrace();
+			throw new SQLException("Connection not possible. This Service not available.");
+		}	
+		}
+
+		/**
+		 * Insert and Delete into virtuoso KB
+		 * @param type
+		 * @param Query
+		 * @throws IOException
+		 */
+		public static void insertDeleteQuery(String Query) throws IOException {
+			VirtGraph virtGraph = new VirtGraph(virtuosoConnection.DB_URL,
+					virtuosoConnection.USER, virtuosoConnection.PASS);
+			/*System.out.println("\nexecute: "+ type +" GRAPH "+ virtuosoConnection.GRAPH
+						+ " { <aa> <bb> 'cc' ."
+						+ " <aa1> <bb1> <123> . "
+						+ "<aa2> <bb2> 456. "
+						+ "}");*/
+			VirtuosoUpdateRequest vur = VirtuosoUpdateFactory
+					.create(Query, virtGraph);
+			vur.exec();
+			virtGraph.close();
+		}
+		/**
+		 * Update a record
+		 * @param deleteOldDataQuery
+		 * @param insertNewDataQuery
+		 * @throws IOException
+		 */
+		public static void updateQuery(String deleteOldDataQuery, String insertNewDataQuery) throws IOException {
+			VirtGraph virtGraph = new VirtGraph(virtuosoConnection.DB_URL,
+					virtuosoConnection.USER, virtuosoConnection.PASS);
+			/*System.out.println("\nexecute: Delete From GRAPH "+ virtuosoConnection.GRAPH
+						+ " { <aa> <bb> 'cc' ."
+						+ " <aa1> <bb1> <123> . "
+						+ "<aa2> <bb2> 456. "
+						+ "}");
+			System.out.println("\nexecute: Insert Into GRAPH "+ virtuosoConnection.GRAPH
+					+ " { <aa3> <bb3> 'cc3' ."
+					+ " <aa4> <bb4> <0123> . "
+					+ "<aa5> <bb5> 0456. "
+					+ "}");*/
+			VirtuosoUpdateRequest vur = VirtuosoUpdateFactory
+					.create(deleteOldDataQuery, virtGraph);
+			vur.exec();
+			vur = VirtuosoUpdateFactory
+					.create(insertNewDataQuery, virtGraph);
+			vur.exec();
+			virtGraph.close();
+		}
+		
+		/**
+		 * Query a graph
+		 * @param query
+		 */
+		public static queryReturnClass query(String query) {
+			VirtGraph virtGraph = new VirtGraph(virtuosoConnection.DB_URL,
+					virtuosoConnection.USER, virtuosoConnection.PASS);
+
+			Query sparql = QueryFactory
+					.create(query);
+			
+			sparql.addGraphURI(virtuosoConnection.GRAPH);
+			
+			VirtuosoQueryExecution vqe = VirtuosoQueryExecutionFactory.create(
+					sparql, virtGraph);
+			
+			queryReturnClass qRC= new queryReturnClass();
+			qRC.setResultSelectVar(sparql.getResultVars());
+			qRC.setReturnedResultSet(vqe.execSelect());
+			qRC.setQuery(sparql);
+			
+			return qRC;
+		}
+
+}
