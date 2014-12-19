1a76b18fdd3f8d0941e24a26280ea7cbf516e65e
congkinh <congkinh.nguyen@inria.fr>
2014-12-19 10:34:20 +0100

    set nobody

--- querymanagerServlet/src/eu/threecixty/querymanager/ThreeCixtyContextListener.java
+++ querymanagerServlet/src/eu/threecixty/querymanager/ThreeCixtyContextListener.java
@@ -13,6 +13,7 @@ import eu.threecixty.profile.GoFlowImpl;
 import eu.threecixty.profile.MobidotImpl;
 import eu.threecixty.profile.RdfFileManager;
 import eu.threecixty.profile.TrayStorage;
+import eu.threecixty.profile.VirtuosoManager;
 import eu.threecixty.querymanager.rest.GoFlowServer;
 import eu.threecixty.querymanager.rest.QueryManagerServices;
 import eu.threecixty.querymanager.rest.CallLogServices;
@@ -46,6 +47,7 @@ public class ThreeCixtyContextListener implements ServletContextListener {
 	    GoFlowServer.setPath(realPath + File.separatorChar + "WEB-INF" + File.separatorChar + "goflow.properties");
 	    DBConnection.getInstance().setPath(realPath);
 	    OAuthWrappers.addScopesByDefault();
+	    VirtuosoManager.getInstance().setNobody();
 	}
 
 }
