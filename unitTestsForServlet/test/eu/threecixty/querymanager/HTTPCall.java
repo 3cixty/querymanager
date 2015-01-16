771ee32b9c213b8b71b42bf78886795f075c545e
congkinh <congkinh.nguyen@inria.fr>
2015-01-16 16:31:49 +0100

    refactoring

--- unitTestsForServlet/test/eu/threecixty/querymanager/HTTPCall.java
+++ unitTestsForServlet/test/eu/threecixty/querymanager/HTTPCall.java
@@ -2,15 +2,29 @@ package eu.threecixty.querymanager;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+import org.json.JSONObject;
 import org.junit.Assert;
 
 public class HTTPCall {
 	
 	private static final String USER_AGENT = "Mozilla/5.0";
+	
+	protected static final String KEY = "687eedc0-17a4-4835-bee6-43ac9394cd04"; // for dev server
+	
+	protected static final String SERVER = "https://dev.3cixty.com/v2/";
 
 	protected void sendPost(String url, String params) throws Exception {
 		sendPost(url, params, null);
@@ -75,5 +89,95 @@ public class HTTPCall {
 			}
 		}
 		in.close();
+	}
+	
+   protected String getAccessToken(String googleAccessToken) throws Exception {
+		
+		HttpURLConnection conn = createConnection(SERVER + "getAccessToken", "GET",
+				new String[] {"google_access_token", "key", "scope"},
+				new String[] {googleAccessToken, KEY, "Profile,Wishlist"});
+		
+		conn.setDoOutput(false);
+		
+		int responseCode = conn.getResponseCode();
+		
+		if (responseCode != 200) Assert.fail();
+		
+		String content = getContent(conn);
+		
+		JSONObject jsonObj = new JSONObject(content);
+		
+		if (jsonObj.has("access_token")) {
+			return jsonObj.getString("access_token");
+		}
+		return null;
+	}
+	
+	protected String getContent(HttpURLConnection conn) throws IOException {
+		return getContent(conn.getInputStream());
+	}
+	
+	protected String getContent(InputStream input) throws IOException {
+		StringBuffer response = new StringBuffer();
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(input));
+		String inputLine;
+		if (response != null) {
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+		}
+		in.close();
+		input.close();
+		return response.toString();
+	}
+	
+	protected HttpURLConnection createConnection(String urlStr, String httpMethod,
+			String [] paramNames, String[] paramValues) throws Exception {
+		HttpURLConnection conn = null;
+		if (urlStr.startsWith("https")) {
+			conn = createConnection(new URL(urlStr));
+		} else {
+			conn =  (HttpURLConnection)(new URL(urlStr)).openConnection();
+		}
+		conn.setRequestMethod(httpMethod.equalsIgnoreCase("POST") ? "POST" : "GET");
+		if (paramNames != null) {
+		    for (int i = 0; i < paramNames.length; i++) {
+		    	conn.setRequestProperty(paramNames[i], paramValues[i]);
+		    }
+		}
+		return conn;
+	}
+	
+	protected HttpsURLConnection createConnection(URL url) throws Exception {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        InputStream caInput = PerformanceTests.class.getResourceAsStream("/3cixty.com.crt");
+        Certificate ca;
+        try {
+            ca = cf.generateCertificate(caInput);
+        } finally {
+            caInput.close();
+        }
+
+        // Create a KeyStore containing our trusted CAs
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        keyStore.load(null, null);
+        keyStore.setCertificateEntry("ca", ca);
+
+        // Create a TrustManager that trusts the CAs in our KeyStore
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        tmf.init(keyStore);
+
+        // Create an SSLContext that uses our TrustManager
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+        
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        
+        conn.setSSLSocketFactory(sslContext.getSocketFactory());
+        return conn;
 	}
 }
