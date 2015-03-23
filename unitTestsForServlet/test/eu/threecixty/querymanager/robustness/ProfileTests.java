b9b605785207520a516fdf709798fefa8a68f77e
congkinh <congkinh.nguyen@inria.fr>
2015-03-23 17:40:52 +0100

    add class name

---
+++ unitTestsForServlet/test/eu/threecixty/querymanager/robustness/ProfileTests.java
@@ -0,0 +1,14 @@
+package eu.threecixty.querymanager.robustness;
+
+/**
+ * 
+ * This test supposes that there is an API to insert UserProfile without checking access token.
+ * The API should be removed after testing.
+ * <br>
+ * Data resulting from this test will be stored in the graph called http://3cixtyTest.com/private/
+ * (check with spe-virtuoso/VirtuosoManager).
+ *
+ */
+public class ProfileTests {
+
+}
