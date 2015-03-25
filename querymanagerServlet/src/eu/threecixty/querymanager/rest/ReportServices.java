ca4523e42c834a5d5a040014d51bae3b6e02bb2a
congkinh <congkinh.nguyen@inria.fr>
2015-03-25 10:59:39 +0100

    removed import unused

--- querymanagerServlet/src/eu/threecixty/querymanager/rest/ReportServices.java
+++ querymanagerServlet/src/eu/threecixty/querymanager/rest/ReportServices.java
@@ -1,7 +1,6 @@
 package eu.threecixty.querymanager.rest;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.Security;
