6c6938b906e68b6579fa6a9283380e12755f5949
congkinh <congkinh.nguyen@inria.fr>
2015-04-07 13:03:29 +0200

    add CE prefix

--- profileStorage/spe/src/eu/threecixty/profile/SparqlEndPointUtils.java
+++ profileStorage/spe/src/eu/threecixty/profile/SparqlEndPointUtils.java
@@ -5,13 +5,19 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLEncoder;
 
+
+
 public class SparqlEndPointUtils {
 	
 	private static final String LOCN_PREFIX = "PREFIX locn: <http://www.w3.org/ns/locn#> ";
+	private static final String CE_MILANO_PREFIX = "PREFIX ce: <http://data.linkedevents.org/cell/milano/>";
 	private static final String SPARQL_ENDPOINT_URL = ProfileManagerImpl.SPARQL_ENDPOINT_URL;
+	
+	//private static final String SPARQL_ENDPOINT_URL = Configuration.getVirtuosoServer() + "/sparql";
 
 	public static void executeQueryViaSPARQL(String query, String format, StringBuilder result) throws IOException {
-		String urlStr = SPARQL_ENDPOINT_URL + URLEncoder.encode(LOCN_PREFIX + query, "UTF-8");
+		String urlStr = SPARQL_ENDPOINT_URL + URLEncoder.encode(LOCN_PREFIX + query, "UTF-8")
+				+ URLEncoder.encode(CE_MILANO_PREFIX + query, "UTF-8");
 		urlStr += "&format=" + URLEncoder.encode(format, "UTF-8");
 
 		URL url = new URL(urlStr);
@@ -24,6 +30,37 @@ public class SparqlEndPointUtils {
 		}
 		input.close();
 	}
+	
+	
+	/*
+	public static void executeQueryViaSPARQL(String query, String format, StringBuilder result) throws IOException {
+		// http://3cixty.eurecom.fr/sparql
+		//URL url = new URL(SPARQL_ENDPOINT_URL);
+		HttpURLConnection.setFollowRedirects(true);
+		URL url = new URL("http://3cixty.eurecom.fr/sparql");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", " application/x-www-form-urlencoded");
+		
+		
+		conn.setDoOutput(true);
+		conn.setDoInput(true);
+		conn.setAllowUserInteraction(true);
+		StringBuilder queryBuilder = new StringBuilder("query=").append(LOCN_PREFIX + query);
+		queryBuilder.append("&format=").append(format);
+		OutputStream output = conn.getOutputStream();
+		output.write(queryBuilder.toString().getBytes("UTF-8"));
+		System.out.println(queryBuilder.toString());
+		output.close();
+		InputStream input = conn.getInputStream();
+		byte [] b = new byte[1024];
+		int readBytes = 0;
+		while ((readBytes = input.read(b)) >= 0) {
+			result.append(new String(b, 0, readBytes, "UTF-8"));
+		}
+		input.close();
+	}
+	*/
 	
 	public static String cleanResultReceivedFromVirtuoso(String result) {
 		return result.replace("\\U", "\\u");
