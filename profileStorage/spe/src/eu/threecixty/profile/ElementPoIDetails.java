c504f0a7fa0daf0eb2b56cdd943df2f25a82f59e
congkinh <congkinh.nguyen@inria.fr>
2015-04-30 21:36:37 +0200

    reuse empty list

--- profileStorage/spe/src/eu/threecixty/profile/ElementPoIDetails.java
+++ profileStorage/spe/src/eu/threecixty/profile/ElementPoIDetails.java
@@ -1,5 +1,6 @@
 package eu.threecixty.profile;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -78,7 +79,7 @@ public class ElementPoIDetails extends ElementDetails {
 		epd.aggregate_rating = this.aggregate_rating;
 		if (language != null && reviewsLanguages != null) epd.reviews = reviewsLanguages.get(language);
 		if (epd.reviews == null) {
-			epd.reviews = new LinkedList <Review>();
+			epd.reviews = Collections.emptyList();
 		}
 		return epd;
 	}
