14181b374af1d15ec86ddefc4a3680756f2bbee1
congkinh <congkinh.nguyen@inria.fr>
2015-05-19 10:55:53 +0200

    add endpoint to get mobidot token

--- querymanagerServlet/src/eu/threecixty/querymanager/rest/MobidotServices.java
+++ querymanagerServlet/src/eu/threecixty/querymanager/rest/MobidotServices.java
@@ -1,6 +1,10 @@
 package eu.threecixty.querymanager.rest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -32,6 +36,8 @@ import eu.threecixty.profile.partners.PartnerAccountUtils;
 public class MobidotServices {
 	
 	private static final String MOBIDOT_SOURCE = "Mobidot";
+	private static final String MOBIDOT_KEY = "SRjHX5yHgqqpZyiYaHSXVqhlFWzIEoxUBmbFcSxiZn58Go02rqB9gKwFqsGx5dks";
+	private static final String MOBIDOT_ENDPOINT = "https://www.movesmarter.nl/external/identitymanager/user/onetimetoken?";
 	
 	private static final Logger LOGGER = Logger.getLogger(
 			MobidotServices.class.getName());
@@ -75,6 +81,65 @@ public class MobidotServices {
 					.type(MediaType.TEXT_PLAIN_TYPE)
 					.build();
 		}
+	}
+	
+	@GET
+	@Path("/getMobidotToken")
+	public Response getMobidotToken(@HeaderParam("access_token") String access_token) {
+		try {
+			long starttime = System.currentTimeMillis();
+			AccessToken userAccessToken = OAuthWrappers.findAccessTokenFromDB(access_token);
+			if (userAccessToken != null && OAuthWrappers.validateUserAccessToken(access_token)) {
+
+				PartnerAccount account = PartnerAccountUtils.retrieveOrAddMobidotUser(userAccessToken.getUid(), userAccessToken.getUid());
+				CallLoggingManager.getInstance().save(userAccessToken.getAppkey(), starttime, CallLoggingConstants.MOBIDOT_GET_USER_SERVICE, CallLoggingConstants.SUCCESSFUL);
+				if (account != null) {
+					if (account.getId() == null) {
+					    setMobidotAccountToUserProfile(userAccessToken.getUid(), account);
+					}
+					String content = getContent(account.getUsername(), account.getPassword());
+					if (content == null) return Response.status(Response.Status.BAD_REQUEST)
+							.entity("Could not get token from Mobidot server")
+							.type(MediaType.TEXT_PLAIN_TYPE)
+							.build();
+					return Response.ok(content, MediaType.APPLICATION_JSON_TYPE).build();
+				} else {
+					return Response.ok("{}", MediaType.APPLICATION_JSON_TYPE).build();
+				}
+			} else {
+				CallLoggingManager.getInstance().save(access_token, starttime, CallLoggingConstants.MOBIDOT_GET_USER_SERVICE, CallLoggingConstants.INVALID_ACCESS_TOKEN + access_token);
+				return Response.status(HttpURLConnection.HTTP_BAD_REQUEST)
+						.entity("The access token is invalid '" + access_token + "'")
+						.type(MediaType.TEXT_PLAIN)
+						.build();
+			}
+		} catch (TooManyConnections e) {
+			return Response.status(Response.Status.BAD_REQUEST)
+					.entity(e.getMessage())
+					.type(MediaType.TEXT_PLAIN_TYPE)
+					.build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			LOGGER.error(e.getMessage());
+			return Response.status(Response.Status.BAD_REQUEST)
+					.entity(e.getMessage())
+					.type(MediaType.TEXT_PLAIN_TYPE)
+					.build();
+		}
+	}
+
+	private String getContent(String username, String password) {
+		try {
+			URL url = new URL(MOBIDOT_ENDPOINT + "userName=" + username + "&password=" + password + "&key=" + MOBIDOT_KEY);
+			InputStream input = url.openStream();
+			
+			input.close();
+		} catch (MalformedURLException e) {
+			LOGGER.error(e.getMessage());
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage());
+		}
+		return null;
 	}
 
 	private void setMobidotAccountToUserProfile(String uid,
