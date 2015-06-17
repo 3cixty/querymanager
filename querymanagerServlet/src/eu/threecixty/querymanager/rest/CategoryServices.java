03c6da8fc9641ed221f8915f3b28d61c8f43c6db
congkinh <congkinh.nguyen@inria.fr>
2015-06-17 17:26:49 +0200

    add category service

---
+++ querymanagerServlet/src/eu/threecixty/querymanager/rest/CategoryServices.java
@@ -0,0 +1,36 @@
+package eu.threecixty.querymanager.rest;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONObject;
+
+import eu.threecixty.oauth.OAuthWrappers;
+import eu.threecixty.profile.elements.CategoryUtils;
+
+@Path("/" + Constants.PREFIX_NAME)
+public class CategoryServices {
+
+	
+	@GET
+	@Path("/getTopCategories")
+	public Response getTopCategories(
+			@HeaderParam("key") String key) {
+		if (!OAuthWrappers.validateAppKey(key)) return Response.status(
+				Response.Status.BAD_REQUEST).entity("Invalid appkey").build();
+		try {
+			List <String> tops = CategoryUtils.getTopCategories();
+			return Response.ok(JSONObject.wrap(tops).toString(),
+					MediaType.APPLICATION_JSON_TYPE).build();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return Response.status(500).build();
+		}
+	}
+}
