afa400db92450a3f4e1391f3ce5d5ba9c7934237
congkinh <congkinh.nguyen@inria.fr>
2015-07-01 11:23:13 +0200

    prepare to reimplement query augmenter to have cleaner code

---
+++ querymanager-java/src/eu/threecixty/querymanager/QueryAugmenter.java
@@ -0,0 +1,25 @@
+package eu.threecixty.querymanager;
+
+/**
+ * This is the interface to augment a query. This interface is just to create a new query which is augmented
+ * from a given original query.
+ *
+ * @author Cong-Kinh Nguyen
+ *
+ */
+public interface QueryAugmenter {
+
+	/**
+	 * Creates a query augmented in string format.
+	 * @param original
+	 * 			The original SPARQL query.
+	 * @param filter
+	 * 			The filter to augment the query.
+	 * @param uid
+	 * 			The 3cixty UID.
+	 * @throws InvalidSparqlQuery
+	 * @return
+	 */
+	String createQueryAugmented(String original, QueryAugmenterFilter filter,
+			String uid) throws InvalidSparqlQuery;
+}
---
+++ querymanager-java/src/eu/threecixty/querymanager/QueryAugmenterImpl.java
@@ -0,0 +1,12 @@
+package eu.threecixty.querymanager;
+
+public class QueryAugmenterImpl implements QueryAugmenter {
+
+	@Override
+	public String createQueryAugmented(String original,
+			QueryAugmenterFilter filter, String uid) throws InvalidSparqlQuery {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
