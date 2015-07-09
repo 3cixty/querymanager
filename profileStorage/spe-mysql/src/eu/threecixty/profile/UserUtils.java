36f14eb90992bfec7f6669126d5d4ee8fac8af2d
congkinh <congkinh.nguyen@inria.fr>
2015-07-09 13:36:35 +0200

    bug fixed

--- profileStorage/spe-mysql/src/eu/threecixty/profile/UserUtils.java
+++ profileStorage/spe-mysql/src/eu/threecixty/profile/UserUtils.java
@@ -264,13 +264,15 @@ public class UserUtils {
 						tmpUids.add(generatedID);
 					}
 				}
-				String userModelSql = "SELECT uid FROM 3cixty_user_profile  WHERE uid IN (:uids)";
-				List <?> userModelList = session.createSQLQuery(userModelSql).setParameterList("uids",
-						tmpUids).list();
-				for (Object obj: userModelList) {
-					String tmpUid = obj.toString();
-					_3cixtyUids.add(tmpUid);
-					accountIdsExisted.add(tmpUid.substring(2));
+				if (tmpUids.size() > 0) {
+					String userModelSql = "SELECT uid FROM 3cixty_user_profile  WHERE uid IN (:uids)";
+					List <?> userModelList = session.createSQLQuery(userModelSql).setParameterList("uids",
+							tmpUids).list();
+					for (Object obj: userModelList) {
+						String tmpUid = obj.toString();
+						_3cixtyUids.add(tmpUid);
+						accountIdsExisted.add(tmpUid.substring(2));
+					}
 				}
 			}
 			
@@ -395,7 +397,7 @@ public class UserUtils {
 		if (!isNullOrEmpty(userProfile.getHasLastCrawlTime())) {
 			userModel.setLastCrawlTimeToKB(Long.parseLong(userProfile.getHasLastCrawlTime()));
 		}
-		//convertKnowsForPersistence(userProfile, userModel);
+		convertKnowsForPersistence(userProfile.getKnows(), userModel);
 		convertAccountsForPersistence(userProfile, userModel, session);
 		convertAccompanyingsForPersistence(userProfile, userModel, session);
 	}
