ac7f2fb7a1b903daa2d2783a4f6c3be73ce9b26d
congkinh <congkinh.nguyen@inria.fr>
2015-09-02 12:06:42 +0200

    add user activation for creating a user and resetting a password

---
+++ 3cixty-db/src/eu/threecixty/userprofile/UserActivation.java
@@ -0,0 +1,68 @@
+package eu.threecixty.userprofile;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "3cixty_user_activation", uniqueConstraints = {
+		@UniqueConstraint(columnNames = {"code"})})
+public class UserActivation implements java.io.Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3909126009057180100L;
+
+	private Integer id;
+	private String code;
+	private ActivationType type;
+	private boolean used;
+	
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Column(name = "code", unique = true, nullable = false, length = 100)
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	@Column(name = "type", unique = false, nullable = false, length = 100)
+	public ActivationType getType() {
+		return type;
+	}
+
+	public void setType(ActivationType type) {
+		this.type = type;
+	}
+
+	@Column(name = "used")
+	public boolean isUsed() {
+		return used;
+	}
+
+	public void setUsed(boolean used) {
+		this.used = used;
+	}
+
+	public enum ActivationType {
+		CREATION, FORGOTTEN_PASSWORD
+	}
+}
