1fc802fc093141129fa494bd992dfc0f81070336
congkinh <congkinh.nguyen@inria.fr>
2015-10-30 16:35:16 +0100

    add try catch

--- 3cixty-monitor/src/eu/threecixty/monitor/MonitorContextListener.java
+++ 3cixty-monitor/src/eu/threecixty/monitor/MonitorContextListener.java
@@ -15,29 +15,33 @@ public class MonitorContextListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent context) {
-		String realPath = context.getServletContext().getRealPath("/");
-		String reportFile = realPath + File.separatorChar + "WEB-INF" + File.separatorChar + "report.properties";
 		try {
-			EmailUtils.loadProperties(reportFile);
-		} catch (IOException e) {
+			String realPath = context.getServletContext().getRealPath("/");
+			String reportFile = realPath + File.separatorChar + "WEB-INF" + File.separatorChar + "report.properties";
+			try {
+				EmailUtils.loadProperties(reportFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			Timer timer = new Timer();
+			AccessTokenChecker tokenChecker = new AccessTokenChecker();
+			tokenChecker.loadProperties(realPath + File.separatorChar + "WEB-INF" + File.separatorChar + "accesstoken.properties");
+			tokenChecker.setTimer(timer);
+
+			NearbyServiceChecker eventsChecker = new NearbyServiceChecker();
+			eventsChecker.loadProperties(realPath + File.separatorChar + "WEB-INF" + File.separatorChar + "nearbyEvents.properties");
+			eventsChecker.setTimer(timer);
+
+			NearbyServiceChecker poIsChecker = new NearbyServiceChecker();
+			poIsChecker.loadProperties(realPath + File.separatorChar + "WEB-INF" + File.separatorChar + "nearbyPoIs.properties");
+			poIsChecker.setTimer(timer);
+
+			eventsChecker.start();
+			poIsChecker.start();
+			tokenChecker.start();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		Timer timer = new Timer();
-		AccessTokenChecker tokenChecker = new AccessTokenChecker();
-		tokenChecker.loadProperties(realPath + File.separatorChar + "WEB-INF" + File.separatorChar + "accesstoken.properties");
-		tokenChecker.setTimer(timer);
-		
-		NearbyServiceChecker eventsChecker = new NearbyServiceChecker();
-		eventsChecker.loadProperties(realPath + File.separatorChar + "WEB-INF" + File.separatorChar + "nearbyEvents.properties");
-		eventsChecker.setTimer(timer);
-		
-		NearbyServiceChecker poIsChecker = new NearbyServiceChecker();
-		poIsChecker.loadProperties(realPath + File.separatorChar + "WEB-INF" + File.separatorChar + "nearbyPoIs.properties");
-		poIsChecker.setTimer(timer);
-		
-		eventsChecker.start();
-		poIsChecker.start();
-		tokenChecker.start();
 	}
 
 }
