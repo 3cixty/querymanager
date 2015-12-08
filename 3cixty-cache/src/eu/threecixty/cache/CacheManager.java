817b7221374eb66ff3aba654754c27725cd76421
congkinh <congkinh.nguyen@inria.fr>
2015-12-08 12:04:01 +0100

    add copyright

--- 3cixty-cache/src/eu/threecixty/cache/CacheManager.java
+++ 3cixty-cache/src/eu/threecixty/cache/CacheManager.java
@@ -1,3 +1,14 @@
+/*===========================================================
+This file is part of the 3cixty platform software.
+
+The 3cixty platform software contains proprietary and confidential information
+of Inria. All rights reserved. Reproduction, adaptation or distribution, in
+whole or in part, is forbidden except by express written permission of Inria.
+Version v2, December 2015.
+Authors: Cong-Kinh Nguyen, Rachit Agarwal, Animesh Pathak.
+Copyright (C) 2015, Inria.
+===========================================================*/
+
 package eu.threecixty.cache;
 
 import java.util.HashMap;
@@ -7,6 +18,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.json.JSONObject;
 
+/**
+ * 
+ * This class is used to store and get caching data.
+ *
+ */
 public class CacheManager {
 	
 	private static final CacheManager instance = new CacheManager();
